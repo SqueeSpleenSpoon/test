5701ab4b8757d33ccdd2a4edc37a17a0c95999bd
Илья Живун <simsoncris@gmail.com>
2021-04-15 16:19:42 +0300

    1213

---
+++ testForGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testForGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
